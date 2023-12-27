36c2015b2806a98e62b403f2ffaf88b73e488fc7
Andreyna Carvalho <andreyna.carvalho@codex.com.br>
2023-12-27 20:18:35 -0300

    feat: finish project

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# gasto-calorico-and-imc
+# 🚀 Gasto Calórico e IMC
 Projeto criado para estudos de Kotlin, nele será possível calcular o IMC e o Gasto Calórico
+
+# Como Rodar?
+
+## Passo 01 - Clonar o projeto
+
+- Clone esse repositório via HTTP:
+
+```
+git clone https://github.com/andreyna1808/gasto-calorico-and-imc.git
+```
+- Clone esse repositório via SSH:
+
+```
+git clone git@github.com:andreyna1808/gasto-calorico-and-imc.git
+```
+
+**OBS.:** Clone o projeto em um local sem caracteres especiais, espaçamentos ou coisas semelhantes, pode atrapalhar na hora de localizar o arquivo para rodar.
+
+## Passo 02 - Abrir o Projeto
+
+- Abra o Projeto em uma IDE, recomendo Android Studio
+
+## Passo 03 - Rodar o Projeto
+
+- Espera a IDE buildar o projeto e então rode ele apertando no play
+
+![playExample](docs/playExample.png)
+
+# 🚀 Exemplo do app desenvolvido
+![gifExample](docs/projetoKotlinResult.gif)
+
+# 📚 Sobre o Projeto
+- Projeto básico para estudos, desenvolvido em Kotlin
+
+[![Desenvolvido em Kotlin](https://skillicons.dev/icons?i=kotlin)](https://skillicons.dev)
+
+[<img src="https://avatars.githubusercontent.com/u/87716793?v=4" width=115><br><sub>Andreyna Carvalho</sub>](https://github.com/andreyna1808)
+
+## Email para contato: andreyna.m.carvalho@gmail.com
--- app/src/main/java/com/example/gasto_calorico_and_imc/CaloricLossActivity.kt
+++ app/src/main/java/com/example/gasto_calorico_and_imc/CaloricLossActivity.kt
@@ -1,21 +1,133 @@
 package com.example.gasto_calorico_and_imc
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Spinner
+import android.widget.TextView
 
 
 class CaloricLossActivity : AppCompatActivity() {
+
+    private lateinit var editAge: EditText
+    private lateinit var editWeight: EditText
+    private lateinit var textResult: TextView
+
+    var selectedActivity = ""
+    var gender = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_caloric_loss)
+
+        editAge = findViewById(R.id.value_age)
+        editWeight = findViewById(R.id.value_weight_caloric)
+        textResult = findViewById(R.id.result_caloric_loss)
 
         val selectPhysicalActivity = findViewById<Spinner>(R.id.selectPhysicalActivity)
         val optionsPhysicalActivities = arrayOf("Leve", "Moderado", "Intenso")
         val adapter = ArrayAdapter(this, R.layout.spinner_item_layout, optionsPhysicalActivities)
         selectPhysicalActivity.adapter = adapter;
+
+        selectPhysicalActivity.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parentView: AdapterView<*>?, selectedItemView: View?, position: Int, id: Long) {
+                selectedActivity = optionsPhysicalActivities[position]
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+        }
     }
 
+    fun selectedMan(view: View) {
+        val imgMan: ImageView = findViewById(R.id.select_man)
+        val imgWoman: ImageView = findViewById(R.id.select_woman)
 
+        imgMan.setImageResource(R.drawable.selected_man)
+        imgWoman.setImageResource(R.drawable.woman)
+        gender = "man"
+    }
+
+    fun selectedWoman(view: View) {
+        val imgMan: ImageView = findViewById(R.id.select_man)
+        val imgWoman: ImageView = findViewById(R.id.select_woman)
+
+        imgMan.setImageResource(R.drawable.man)
+        imgWoman.setImageResource(R.drawable.selected_woman)
+        gender = "woman"
+    }
+
+    private fun basalEnergetic(age: Int, gender: String, weight: Double): Double {
+        // man
+        if (age in 1..2 && gender == "man") {
+            return (59.512 / 1000 * weight) - (304 / 10)
+
+        } else if (age in 3..9 && gender == "man") {
+            return (22706 / 1000 * weight) + (5043 / 10)
+        }
+        else if (age in 10..17 && gender == "man") {
+            return (17686 / 1000 * weight) + (6582 / 10)
+        }
+        else if (age in 18..30 && gender == "man") {
+            return (15057 / 1000 * weight) + (69224 / 100)
+        }
+        else if (age in 31..60 && gender == "man") {
+            return (11472 / 1000  * weight) + (8731 / 10)
+        }
+        else if (age > 60 && gender == "man") {
+            return (11711 / 1000 * weight) + (5877 / 10)
+        }
+
+        // woman
+        else if (age in 1..2 && gender == "woman") {
+            return (58317 / 1000 * weight) - (311 / 10)
+
+        } else if (age in 3..9 && gender == "woman") {
+            return (20315 / 1000 * weight) + (4859 / 10)
+
+        }
+        else if (age in 10..17 && gender == "woman") {
+            return (13384 / 1000 * weight) + (6926 / 10)
+
+        }
+        else if (age in 18..30 && gender == "woman") {
+            return (14818 / 1000 * weight) + (4866 / 10)
+
+        }
+        else if (age in 31..60 && gender == "woman") {
+            return (8126 / 1000 * weight) + (8456 / 10)
+
+        }
+        else if (age > 60 && gender == "woman") {
+            return (9082 / 1000  * weight) + (6585 / 10)
+
+        } else {
+            return 0.0
+        }
+    }
+
+    private fun activityFactor(basalValue: Double): Double {
+        if (selectedActivity == "Leve") {
+            return basalValue * 1.55
+        } else if (selectedActivity == "Moderado") {
+            return basalValue * 1.84
+        } else {
+            return basalValue * 2.2
+        }
+    }
+
+    fun calculateCaloricLoss(view: View) {
+        val age = editAge.text.toString().toIntOrNull() ?: 0
+        val weight = editWeight.text.toString().toDoubleOrNull() ?: 0.0
+
+        val resultBasalEnergetic = basalEnergetic(age, gender, weight)
+        val resultActivityFactor = activityFactor(resultBasalEnergetic)
+
+        textResult.text = "Seu gasto calórico estimado é: ${resultActivityFactor.toInt()}kcal por dia"
+        textResult.setTextColor(Color.parseColor("#0000FF"));
+    }
 }
